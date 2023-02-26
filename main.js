2cf9f69a3e379901dc4baf3253c3aab1423e2a36
Rivenshell <rivenshell111@gmail.com>
2023-02-26 01:11:32 -0500

    Torus

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer() ({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
 
@@ -15,3 +15,28 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
 
 renderer.render(scene, camera)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({ color: 0xF2ACB9 })
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(25, 25, 25)
+
+scene.add(pointLight)
+
+
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+
+  torus.rotation.x += 0.01
+   torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+}
+
+animate()
